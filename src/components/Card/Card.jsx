b335a876d8e8c87b5d747ce788a033e3cdc4bcc2
Piotrek W <pwojciechowski2014@gmail.com>
2022-09-26 19:16:05 +0200

    get data for detail page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Card = ({ infos }) => {
     return (
         <div>
             <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${infos.id}.png`} alt={infos.name} />
             <h2>{infos.name}</h2>
             <span>{infos.type}</span>
-            <button>Details</button>
+            <Link to={`/${infos.id}`}>Details</Link>
         </div>
     );
 };
--- src/pages/PokemonDetails/PokemonDetails.jsx
+++ src/pages/PokemonDetails/PokemonDetails.jsx
@@ -3,9 +3,35 @@ import { useParams } from 'react-router-dom';
 
 const GET_POKEMON = gql`
     query PokemonById($id: Int!) {
-        pokemon_v2_pokemon_by_pk(id: $id) {
+        pokemon: pokemon_v2_pokemon_by_pk(id: $id) {
             id
             name
+            stats: pokemon_v2_pokemonstats {
+                id
+                base_stat
+                stat: pokemon_v2_stat {
+                    name
+                    char: pokemon_v2_characteristics {
+                        charDesc: pokemon_v2_characteristicdescriptions(where: { language_id: { _in: [5, 6, 7, 9] } }) {
+                            id
+                            description
+                            language_id
+                            lang: pokemon_v2_language {
+                                name
+                            }
+                        }
+                    }
+                }
+            }
+            types: pokemon_v2_pokemontypes {
+                type: pokemon_v2_type {
+                    id
+                    name
+                    generation: pokemon_v2_generation {
+                        name
+                    }
+                }
+            }
         }
     }
 `;
@@ -15,13 +41,39 @@ const PokemonDetails = () => {
     const { loading, error, data } = useQuery(GET_POKEMON, {
         variables: { id },
     });
+    console.log(data);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
 
     return (
         <div>
-            <h2>{data.pokemon_v2_pokemon_by_pk.name}</h2>
+            <p>{data.pokemon.id}</p>
+            <h2>{data.pokemon.name}</h2>
+            {data.pokemon.types.map((item) => (
+                <div key={item.type.id}>
+                <p>{item.type.name}</p>
+                <p>{item.type.generation.name}</p>
+                </div>
+            ))}
+            <h3>Stats</h3>
+            {data.pokemon.stats.map((item) => (
+                <div key={item.id}>
+                    <p>{item.stat.name}</p>
+                    <p>{item.base_stat}</p>
+                </div>
+            ))}
+            <h3>Description</h3>
+            {data.pokemon.stats.map((item) =>
+                item.stat.char.map((item) =>
+                    item.charDesc.map((item) => (
+                        <div key={item.id}>
+                            {' '}
+                            <p>{item.description}</p>{' '}
+                        </div>
+                    ))
+                )
+            )}
         </div>
     );
 };
