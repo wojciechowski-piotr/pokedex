2b1884931c70326afc20dccac1fb8ce91de4d08f
Piotrek W <pwojciechowski2014@gmail.com>
2022-09-27 19:16:06 +0200

    style for Nav component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
 import Logo from './../../assets/pokemon_logo.png';
+import styles from './Nav.module.scss';
 
 const Nav = () => {
     return (
-        <div>
-            <img src={Logo} alt="Pokemon App" />
+        <div className={styles.container}>
+            <Link to="/" className={styles.link}>
+                <img src={Logo} alt="Pokemon App" />
+            </Link>
         </div>
     );
 };
